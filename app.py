95f437976fd4ade151d9ff8cf75f3ff76f6e4e67
Galina958 <popova.galina99@mail.ru>
2022-03-17 22:34:54 +0300

    3

--- app.py
+++ app.py
@@ -26,3 +26,10 @@ def sum(request, response, num_1, num_2):
     total = int(num_1) + int(num_2)
     response.text = f"{num_1} + {num_2} = {total}"
 
+@app.route("/book")
+class BooksResource:
+    def get(self, req, resp):
+        resp.text = "Books Page"
+
+    def post(self, req, resp):
+        resp.text = "Endpoint to create a book"
